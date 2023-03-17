4efde103f1c246ba572a5ad7ef98266454eaadb8
MarioBlasi <mblasi509@gmail.com>
2023-03-17 14:11:13 +0000

    cells color

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,36 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed
 // emetto un messaggio in console con il numero della cella cliccata.
+
+/* Creare una griglia1 0x10
+
+ Ogni volta che clicco su un quadrato si colora di azzurro */
+
+// Creo una variabile dove salvo il container delle 100 celle (container-grid)
+
+const container = document.querySelector(".container-main");
+
+// creo un max numero di celle
+
+let nMax = 100;
+// creo un ciclo n volte per le celle (dove n in questo caso sta per 100)
+
+for (let i = 0; i < nMax; i++) {
+  const cell = `<div class="cell"></div>`;
+  container.innerHTML += cell;
+}
+
+// seleziono una cella che ha classe cell e active
+
+const cellEl = document.querySelectorAll(".cell");
+
+// aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
+for (let i = 0; i < cellEl.length; i++) {
+  const thisCell = cellEl[i];
+  console.log(thisCell);
+  thisCell.addEventListener("click", function () {
+    thisCell.classList.toggle("bg_blue");
+    // this.classList.add("active")
+    console.log("Changed the color");
+  });
+}
