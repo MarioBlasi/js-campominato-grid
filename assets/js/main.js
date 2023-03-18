55d737ff74af2fdb9175e197ec2a71a3d936c76a
MarioBlasi <mblasi509@gmail.com>
2023-03-18 10:50:01 +0000

    play and start

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,13 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed
 // emetto un messaggio in console con il numero della cella cliccata.
 
+// const button = document.getElementById("crea-griglia");
+// const playButton = document.getElementById("play");
+// const griglia = document.getElementById("griglia");
+
 const container = document.querySelector(".container-main");
 
-const button = document.getElementById("crea-griglia");
-const playButton = document.getElementById("play");
-const griglia = document.getElementById("griglia");
-
-let gameStarted = false;
-
 let nMax = 100;
-
-// Funzione che avvia il gioco
-function startGame() {
-  console.log("starGame");
-}
 
 for (let i = 1; i <= nMax; i++) {
   const cell = `<div class="cell">${i}</div>`;
@@ -26,11 +19,16 @@ for (let i = 1; i <= nMax; i++) {
 
 const cellEl = document.querySelectorAll(".cell");
 
-for (let i = 0; i < cellEl.length; i++) {
-  const thisCell = cellEl[i];
-  console.log(thisCell);
-  thisCell.addEventListener("click", function () {
-    thisCell.classList.toggle("bg_blue");
-    console.log("Changed the color" + thisCell.textContent);
-  });
+function startGame() {
+  for (let i = 0; i < cellEl.length; i++) {
+    const thisCell = cellEl[i];
+    console.log(thisCell);
+    thisCell.addEventListener("click", function () {
+      thisCell.classList.toggle("bg_blue");
+      console.log("Changed the color" + thisCell.textContent);
+    });
+  }
 }
+const playButton = document.getElementById("play-button");
+
+playButton.addEventListener("click", startGame);
