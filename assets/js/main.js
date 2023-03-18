92c623bec8f447ea9267dbce8e5ee99246d8fca1
MarioBlasi <mblasi509@gmail.com>
2023-03-18 20:25:22 +0000

    menu a discesa

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,21 @@ function startGame() {
 const playButton = document.getElementById("play-button");
 
 playButton.addEventListener("click", startGame);
+
+/*Bonus
+Aggiungere una select accanto al bottone di generazione, 
+che fornisca una scelta tra tre diversi livelli di difficoltà:
+con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100,
+ divise in 10 caselle per 10 righe;
+con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81,
+ divise in 9 caselle per 9 righe;
+con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, 
+divise in 7 caselle per 7 righe;*/
+
+//------------//
+
+/*per aggiungere la selezione livello di difficoltá utilizziamo in HTML
+  un menu a discesa (bootstrap) */
+
+/* utiliziamo un function per generare il gioco 
+   - utilizzando if else per assegnare i valori*/
